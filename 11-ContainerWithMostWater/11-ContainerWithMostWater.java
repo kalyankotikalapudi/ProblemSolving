15701890fda5a0551a8ac2947dc38992d16dd9dc
kalyankotikalapudi <60501057+kalyankotikalapudi@users.noreply.github.com>
2025-01-16 22:21:19 -0700

    Update file 1/16/2025, 10:22:06 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,40 +1,19 @@
 class Solution {
     public int maxArea(int[] height) {
-        
-        int i =0;
-        int j = (height.length)-1;
-        int find=0;
-        int width=j-i;
-       
-       
-
-       while(i<j)
-       {
-
-           int output= width*(Math.min(height[i], height[j]));
-             find = Math.max(find,output);
-               if(height[i]<height[j])
-               {  i++;
-              
-               }
-
-            else{j--;  }
-
-            width=j-i;
-            
-           
-
-           
-
-         
-
-
-
-           
-
-         
-           
-       }
-       return find;
+        int total=0;
+        int i=0;
+        int j=height.length-1;
+        while(i<j){
+            int width=j-i;
+            int current = Math.min(height[i],height[j])*width;
+            total =Math.max(total,current);
+            if(height[i]<height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return total;
     }
 }
