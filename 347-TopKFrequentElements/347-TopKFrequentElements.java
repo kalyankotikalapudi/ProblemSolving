5d0c3d519c898fdba26dd18de92cf7ea97ac70a1
kalyankotikalapudi <60501057+kalyankotikalapudi@users.noreply.github.com>
2025-01-20 17:43:21 -0700

    Update file 1/20/2025, 5:44:09 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -3,16 +3,12 @@ class Solution {
 
        Map<Integer , Integer> map = new HashMap<>();
 
-       for(int i=0;i<nums.length;i++)
+       for(int i:nums)
        {
-           if(map.containsKey(nums[i]))
-           {
-                 map.put(nums[i], map.get(nums[i])+1);
-           }
-           else
-           {
-                 map.put(nums[i], 1);
-           }
+          
+                 map.put(i, map.getOrDefault(i,0)+1);
+           
+         
           
 
        }
