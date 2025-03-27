963949a187d3c7323f85dfffb67312dd996ce444
kalyankotikalapudi <60501057+kalyankotikalapudi@users.noreply.github.com>
2025-03-26 20:46:49 -0500

    File updated at 3/26/2025, 8:46:49 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,4 +1,4 @@
-// Last updated: 3/26/2025, 8:45:27 PM
+// Last updated: 3/26/2025, 8:46:49 PM
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,39 +10,83 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-   
-        ListNode l3= new ListNode(0);
-        ListNode current=l3;
-        
-        ListNode first = l1;
-        ListNode second = l2;
-        int carry=0;
 
-        while(first!=null || second!=null)
+    public ListNode reverse(ListNode head)
+    {
+
+         if(head==null)
         {
-            int x =0; int y=0;
-            
-            if(first!=null){x=first.val;}else{x=0;}
-            if(second!=null){y=second.val;}else{y=0;}
-           
-           
-            int sum =carry+ x+y;
-            carry= sum/10;
-            sum=sum%10;
-            
-            current.next = new ListNode(sum);
-            current=current.next;
-            if(first!=null){first=first.next;}
-            if(second!=null){second=second.next;}
+            return null;
         }
 
+        ListNode current=head;
+        ListNode prev=null;
+        ListNode front =null;
 
-       if (carry > 0) {
-        current.next = new ListNode(carry); 
-       }
 
-        
-        return l3.next;
+        while(current!=null)
+        {
+            front=current.next;
+            current.next=prev;
+            prev=current;
+            current= front;
+        }
+        head = prev;
+
+        return head;
+
+
+    }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        // ListNode x = reverse(l1);
+         //ListNode y = reverse(l2);   
+
+         ListNode x1 = l1;
+         ListNode y1 =l2;
+
+         ListNode result = new ListNode(0);
+         ListNode current=result;
+
+         int a =0;
+         int carry=0;
+         
+         while(x1!=null || y1!=null || carry!=0 )
+         { 
+            int xval; 
+            if (x1 != null) {xval = x1.val;  }
+             else {xval = 0;  }
+
+            int yval; 
+            if (y1 != null) {yval = y1.val;  } 
+            else {yval = 0;  }
+
+              a= xval+yval+carry;
+              carry= a /10;
+              a=a%10;
+
+              current.next=new ListNode(a);
+              current=current.next;
+             
+            
+            
+             if(x1!=null){x1=x1.next;}
+             if(y1!=null){y1=y1.next;}
+
+            
+             //result = result.next;
+
+             
+            
+              
+         }
+
+
+
+
+         return result.next;
+       
     }
 }
